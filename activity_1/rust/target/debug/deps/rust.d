a2ec833a7b891e9310c8576b088f0f8b297895ee
Scapsters <scotty.happy@gmail.com>
2025-01-27 09:07:37 -0500

    rename assignment_1

--- activity_1/rust/target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\scott\Code\School\swen_342\activity_1\rust\target\debug\deps\rust.exe: src\main.rs
-
-C:\Users\scott\Code\School\swen_342\activity_1\rust\target\debug\deps\rust.d: src\main.rs
-
-src\main.rs:
--- activity_1/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\scott\Code\School\swen_342\activity_1\rust\target\debug\rust.exe: C:\Users\scott\Code\School\swen_342\activity_1\rust\src\main.rs
