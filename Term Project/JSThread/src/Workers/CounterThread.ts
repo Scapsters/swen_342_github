6c2bf8f2948b8d74bce6fdb605956e805f0bbc9e
Scapsters <scotty.happy@gmail.com>
2025-02-28 19:45:42 -0500

    add comment

--- Term Project/JSThread/src/Workers/CounterThread.ts
+++ Term Project/JSThread/src/Workers/CounterThread.ts
@@ -1,7 +1,7 @@
 import Thread, { initThread } from "./WorkerThread";
 
 class CounterWorker extends Thread {
-
+    // Empty for now. Thread is quite extensible so this should be a small class compared to it.
 }
 
 initThread()
