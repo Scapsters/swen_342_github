0292a48322f947d4c029a52f83ac56787506e7a6
Scapsters <scotty.happy@gmail.com>
2025-04-02 07:10:59 -0400

    implement part 3

--- activity-2_Woolies/src/main.rs
+++ activity-2_Woolies/src/main.rs
@@ -11,27 +11,23 @@ mod bridge;
 use crate::bridge::Bridge;
 
 use rand::Rng;
-use std::thread;
+use std::thread::JoinHandle;
 
 fn main() {
     let mut rng = rand::thread_rng();
 
-    let woolies: Vec<_> = (1..=3)
-        .map(|i| Woolie::new(i.to_string(), rng.gen_range(3..=6), City::random()))
+    let bridge = Bridge::new(3);
+    let threads: Vec<JoinHandle<()>> = (1..=10)
+        .map(|i| {
+            bridge.request_key(Woolie::new(
+                i.to_string(),
+                rng.gen_range(3..=6),
+                City::random(),
+            ))
+        })
         .collect();
 
-    let threads: Vec<_> = woolies
-        .into_iter()
-        .map(|woolie| build_thread(move || woolie.run()))
-        .collect();
-
-    threads
-        .into_iter()
-        .for_each(|thread| thread.join().unwrap());
-}
-
-fn build_thread<T: FnOnce() + Send + 'static>(function: T) -> thread::JoinHandle<()> {
-    thread::Builder::new()
-        .spawn(|| function())
-        .expect("Failed to create thread")
+    threads.into_iter().for_each(|t| {
+        let _ = t.join();
+    });
 }
--- activity-2_Woolies/src/woolie.rs
+++ activity-2_Woolies/src/woolie.rs
@@ -1,12 +1,12 @@
-use std::thread;
+use std::{sync::{Arc, Mutex}, thread};
 
 use crate::city::City;
 
-#[derive(Clone)]
+#[derive(PartialEq, Clone)]
 pub struct Woolie {
-    name: String,
-    time: u32,
-    destination: City,
+    pub name: String,
+    pub time: u32,
+    pub destination: City,
 }
 
 impl Woolie {
@@ -17,28 +17,14 @@ impl Woolie {
             destination,
         }
     }
-
-    pub fn run(&self) {
-        println!(
-            "Woolie has arrived at bridge. Destination: {}",
-            self.destination.to_string()
-        );
-        thread::sleep(std::time::Duration::from_secs(1));
-        self.cross_bridge();
-        println!(
-            "Woolie {} has arrived at {}",
-            self.name, self.destination.to_string()
-        );
-    }
-
+    
     pub fn cross_bridge(&self) {
         println!(
             "Woolie {} is starting to cross the bridge for {} seconds",
             self.name, self.time
         );
-        for i in 0..=self.time {
+        for _ in 0..=self.time {
             thread::sleep(std::time::Duration::from_secs(1));
-            println!("Woolie {} is {} seconds in", self.name, i);
         }
     }
 }
